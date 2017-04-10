8e08de43d964209564ccf0eb4d18205059e8c32a
maksmeller <maksmeller123@gmail.com>
2017-04-10 23:02:22 +0200

    Program główny
    Dołączenie progarmu głównego do projektu

---
+++ STM/main.c
@@ -0,0 +1,16 @@
+#include "PedometrLib.h"
+
+int main(void)
+{
+	SystemInit();
+    LedOutInit(GPIOD,GPIO_Pin_12,1);
+    LedOutInit(GPIOD,GPIO_Pin_13,0);
+    USART3Init(9600);
+    Tim3Init(10000,840);
+    LisInit();
+
+    while(1)
+    {
+
+    }
+}
